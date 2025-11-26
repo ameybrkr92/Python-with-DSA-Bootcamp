5202d14c92a7ba425717b6c53f5864ff2a35b657
Amey Borker <ameyborker_22@yahoo.co.in>
2025-11-26 23:34:39 +0530

    LinkedList append

--- 2. Linked_List/4.LL_append.py
+++ 2. Linked_List/4.LL_append.py
@@ -12,11 +12,21 @@ class Linkedlist:
         
     def print_list(self):
         temp = self.head
-        while temp is None:
+        while temp is not None:
             print(temp.value)
             temp = temp.next
             
     def append(self, value):
         new_node = Node(value)
-        if self.length==0
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
         
+my_linked_list = Linkedlist(2)
+my_linked_list.print_list()
+my_linked_list.append(4)
+my_linked_list.print_list()
