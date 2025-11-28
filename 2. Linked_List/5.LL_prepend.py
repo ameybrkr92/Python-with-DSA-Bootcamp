c9ee050e5c3054f281fa8d2824fc15e6ec71688e
Amey Borker <ameyborker_22@yahoo.co.in>
2025-11-28 23:06:11 +0530

    prepend & pop first

---
+++ 2. Linked_List/5.LL_prepend.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+class Linkedlist:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+        
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+            
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+        
+    def prepend (self, value):
+        new_node = Node(value)
+        if self.length == 0 :
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length +=1
+        return True
+            
+        
+        
+        
+                
+my_linked_list = Linkedlist(2)
+my_linked_list.print_list()
+my_linked_list.append(4)
+my_linked_list.print_list()
